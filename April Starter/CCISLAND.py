14915e8b25a9abf0248a7771ffa85a8698370dc6
ankansharma <ankanroni3@gmail.com>
2021-04-26 10:24:47 +0530

    CCISLAND updated

--- April Starter/CCISLAND.py
+++ April Starter/CCISLAND.py
@@ -1,10 +1,10 @@
 # Contest START3C
-# Qestion CCISLAND
+# Question CCISLAND
 
 t = int(input())
 for i in range(t):
-    lst = list(map(int,input().split()))
-    if min(lst[0]//lst[2],lst[1]//lst[3]) >= lst[4]:
+    list_of_input = list(map(int, input().split()))
+    if min(list_of_input[0] // list_of_input[2], list_of_input[1] // list_of_input[3]) >= list_of_input[4]:
         print("YES")
     else:
         print("NO")
